f5258129fd7d32c32bec08e777e3a57a79db505b
Amber Shipley <ashipley2@me.com>
2026-05-05 11:09:15 -0400

    Update file hierarchy related to firebase

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { db } from '@/lib/firebase'
+import { db } from '@/lib/firebase/firebase'
 import { collection, addDoc } from 'firebase/firestore'
 import { getCurrentUser } from '@/lib/firebase/user'
 
--- src/lib/firebase/use_auth.js
+++ src/lib/firebase/use_auth.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '@/lib/firebase'
+import { auth } from '@/lib/firebase/firebase'
 
 export function use_auth() {
   const [user, set_user] = useState(undefined)
