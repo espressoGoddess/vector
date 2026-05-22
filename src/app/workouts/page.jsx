4e45000e2138bb3381358332036f08af93063619
Amber Shipley <ashipley2@me.com>
2026-05-22 15:30:12 -0400

    Create workouts page]

---
+++ src/app/workouts/page.jsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useAuth } from '@/lib/firebase/useAuth'
+
+export default function Page() {
+	const user = useAuth()
+	const router = useRouter()
+
+	const [loadingWorkouts, setLoadingWorkouts] = useState(true)
+	const [pastWorkouts, setPastWorkouts] = useState(null)
+
+	useEffect(() => {
+		if (user === undefined) return
+
+		if (!user) {
+			router.push('/login')
+			return
+		}
+
+		setLoadingWorkouts(false)
+	}, [user, router])
+
+	if (loadingWorkouts) {
+		return <main className="pt-20">Loading...</main>
+	}
+
+	if (!user) return null
+
+	return (
+		<div className="p-12">
+			<h1 className="mt-8 ml-8 text-xl">Workouts</h1>
+		</div>
+	)
+}
