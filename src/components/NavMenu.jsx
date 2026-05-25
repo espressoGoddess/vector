a23cd4367f5e5ac59b5c3a96170e288249b927b3
Amber Shipley <ashipley2@me.com>
2026-05-25 11:36:15 -0400

    Make NavBar sticky so it persists at the top of each page

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function NavMenu() {
   return (
-    <nav className="border-b p-4 flex gap-4 justify-around">
+    <nav className="border-b p-4 flex gap-4 justify-around sticky top-0 z-50 bg-background">
       <Link href="/">Home</Link>
       <Link href="/workouts">Workouts</Link>
       <Link href="/goals">Goals</Link>
